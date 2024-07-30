fa154093a4dd0e3b63d8c12b1125be309a05643b
runalipawar <168290899+runalipawar@users.noreply.github.com>
2024-07-31 05:15:43 +0530

    Update urls.py

--- hospital/hospital_pro/hospital_app/urls.py
+++ hospital/hospital_pro/hospital_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import signup, login_view, patient_dashboard, doctor_dashboard
+from .views import index, login_view, patient_dashboard, doctor_dashboard
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
+    path('index/', index, name='index'),
     path('login/', login_view, name='login'),
     path('patient_dashboard/', patient_dashboard, name='patient_dashboard'),
     path('doctor_dashboard/', doctor_dashboard, name='doctor_dashboard'),
